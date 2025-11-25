a3a8873f8214862f535e79f39f78d68c6e72048b
BeastTheNinja <BeastTheNinja21@outlook.com>
2025-11-25 10:57:48 +0100

    new DB JS and prisma without ts is bad use 6.19 versions

---
+++ prisma/migrations/20251125095622_update_db/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE `cars` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `category` VARCHAR(191) NOT NULL,
+    `brand` VARCHAR(191) NOT NULL,
+    `model` VARCHAR(191) NOT NULL,
+    `year` INTEGER NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `fuelType` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `categories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `brands` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `logo` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
